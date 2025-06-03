da6d1ddc7f07c9eb02f83ca4b319f5c73876781c
Luo051227 <133470063+Luo051227@users.noreply.github.com>
2025-06-03 15:53:24 +0800

    Update 07.py

--- homework/Python 基礎練習/07.py
+++ homework/Python 基礎練習/07.py
@@ -1,1 +1,12 @@
+def dict_to_string(d):
+    parts = []
 
+    for key, value in d.items():
+        part = f"{str(key)}:{str(value)}"
+        parts.append(part)
+
+    result = ', '.join(parts)
+    return result
+
+
+print(dict_to_string({'a': 1, 'b': 2})) #參考gpt後修改
