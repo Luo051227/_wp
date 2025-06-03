fdbedca63bfbeae5d5da29e20717c8d419134f6b
Luo051227 <133470063+Luo051227@users.noreply.github.com>
2025-06-03 16:36:04 +0800

    Update 09.py

--- homework/Python 基礎練習/09.py
+++ homework/Python 基礎練習/09.py
@@ -1,1 +1,13 @@
+def most_common(nums):
+    count = {}
+    for num in nums:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
 
+    most = max(count, key=count.get)
+    return most
+
+
+print(most_common([1, 2, 2, 3, 3, 3, 4])) #gpt
